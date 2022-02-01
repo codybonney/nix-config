8ccf548ef6385b3cd6ed0db63dfa78f74053ea52
Cody Bonney <me@codybonney.com>
2022-02-01 12:39:09 -0700

    Added website to networking hosts

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -35,6 +35,9 @@
 
   # networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.hosts = {
+    "192.184.90.52" = [ "beta.codybonney.com" ];
+  };
 
   # Set your time zone.
   time.timeZone = "America/Denver";
